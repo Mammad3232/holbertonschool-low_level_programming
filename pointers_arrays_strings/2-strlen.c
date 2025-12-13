965024db6ba8044749e67ec2d268603f359c2ccf
Mammad3232 <ciraqzadmmmd@gmail.com>
2025-12-13 13:23:15 +0000

    Task2

--- pointers_arrays_strings/2-strlen.c
+++ pointers_arrays_strings/2-strlen.c
@@ -1,15 +1,22 @@
 #include "main.h"
 
-int _strlen(char *s){
-   
-    int length = 0;
+/**
+ * _strlen - returns the length of a string
+ * @s: pointer to the string
+ *
+ * Return: the length of the string
+ */
+int _strlen(char *s)
+{
+	/* Initialize length to 0 */
+	int length = 0;
 
-    while (*s != '\0')  
-    {
-        length++;  
-        s++;       
-    }
+	/* Traverse the string until the null terminator */
+	while (*s != '\0')
+	{
+		length++;  /* Increment the length for each character */
+		s++;       /* Move to the next character */
+	}
 
-    return length;  
+	return (length);  /* Return the length of the string */
 }
-
